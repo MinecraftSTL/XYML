HMCL-dev/HMCL

提早加载配置文件 (#6420)

HMCL/src/main/java/org/jackhuang/hmcl/EntryPoint.java
@@ -17,21 +17,28 @@
  */
 package org.jackhuang.hmcl;
 
+import org.jackhuang.hmcl.setting.SambaException;
+import org.jackhuang.hmcl.setting.SettingsManager;
 import org.jackhuang.hmcl.util.FileSaver;
 import org.jackhuang.hmcl.util.SelfDependencyPatcher;
 import org.jackhuang.hmcl.util.SwingUtils;
 import org.jackhuang.hmcl.java.JavaRuntime;
 import org.jackhuang.hmcl.util.io.FileUtils;
 import org.jackhuang.hmcl.util.io.JarUtils;
+import org.jackhuang.hmcl.util.platform.CommandBuilder;
 import org.jackhuang.hmcl.util.platform.OperatingSystem;
 
 import javax.swing.JOptionPane;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
 import java.io.IOException;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.concurrent.CancellationException;
 
 import static org.jackhuang.hmcl.util.logging.Logger.LOG;
@@ -64,6 +71,17 @@ public final class EntryPoint {
         verifyJavaFX();
         addEnableNativeAccess();
         enableUnsafeMemoryAccess();
+
+        try {
+            SettingsManager.init();
+        } catch (SambaException e) {
+            showWarning(i18n("fatal.samba"));
+        } catch (IOException e) {
+            LOG.error("Failed to load config", e);
+            checkConfigOwner();
+            SwingUtils.showErrorDialog(i18n("fatal.config_loading_failure", SettingsManager.localConfigDirectory()));
+            EntryPoint.exit(1);
+        }
 
         Launcher.main(args);
     }
@@ -225,15 +243,8 @@ public final class EntryPoint {
 
     private static void checkWine() {
         if (OperatingSystem.isRunningUnderWine()) {
-            SwingUtils.initLookAndFeel();
             LOG.warning("HMCL is running under Wine or its distributions!");
-
-            int result = JOptionPane.showOptionDialog(null, i18n("fatal.wine_warning"), i18n("message.warning"), JOptionPane.OK_CANCEL_OPTION,
-                    JOptionPane.WARNING_MESSAGE, null, null, null);
-
-            if (result == JOptionPane.CANCEL_OPTION || result == JOptionPane.CLOSED_OPTION) {
-                exit(1);
-            }
+            showWarning(i18n("fatal.wine_warning"));
         }
     }
 
@@ -270,6 +281,70 @@ public final class EntryPoint {
             } catch (Throwable e) {
                 LOG.warning("Failed to enable unsafe memory access", e);
             }
+        }
+    }
+
+    private static void checkConfigOwner() {
+        if (OperatingSystem.CURRENT_OS == OperatingSystem.WINDOWS)
+            return;
+
+        String userName = System.getProperty("user.name");
+        Path configDirectory = SettingsManager.localConfigDirectory();
+        if (!Files.exists(configDirectory)) {
+            return;
+        }
+
+        String owner;
+        try {
+            owner = Files.getOwner(configDirectory).getName();
+        } catch (IOException ioe) {
+            LOG.warning("Failed to get file owner", ioe);
+            return;
+        }
+
+        if (Files.isWritable(configDirectory) || userName.equals("root") || userName.equals(owner))
+            return;
+
+        ArrayList<String> files = new ArrayList<>();
+        files.add(configDirectory.toString());
+        if (Files.exists(Metadata.HMCL_USER_HOME))
+            files.add(Metadata.HMCL_USER_HOME.toString());
+
+        Path mcDir = Paths.get(".minecraft").toAbsolutePath().normalize();
+        if (Files.exists(mcDir))
+            files.add(mcDir.toString());
+
+        String command = new CommandBuilder().addAll("sudo", "chown", "-R", userName).addAll(files).toString();
+        SwingUtils.initLookAndFeel();
+
+        Object[] options = {i18n("button.copy_and_exit"), i18n("button.cancel")};
+        int result = JOptionPane.showOptionDialog(null,
+                i18n("fatal.config_loading_failure.unix", owner, command),
+                i18n("message.error"),
+                JOptionPane.DEFAULT_OPTION,
+                JOptionPane.ERROR_MESSAGE,
+                null,
+                options,
+                options[0]);
+
+        if (result == 0) {
+            try {
+                Toolkit.getDefaultToolkit().getSystemClipboard().setContents(new StringSelection(command), null);
+            } catch (Throwable e) {
+                LOG.warning("Failed to copy command to clipboard", e);
+            }
+        }
+        EntryPoint.exit(1);
+    }
+
+    private static void showWarning(String message) {
+        SwingUtils.initLookAndFeel();
+
+        int result = JOptionPane.showOptionDialog(null, message, i18n("message.warning"), JOptionPane.OK_CANCEL_OPTION,
+                JOptionPane.WARNING_MESSAGE, null, null, null);
+
+        if (result == JOptionPane.CANCEL_OPTION || result == JOptionPane.CLOSED_OPTION) {
+            exit(1);
         }
     }
 

MinecraftSTL/XYML

XYML/src/main/java/space/minecraftstl/xyml/EntryPoint.java
@@ -20,8 +20,12 @@ package space.minecraftstl.xyml;
 import space.minecraftstl.xyml.util.FileSaver;
 import space.minecraftstl.xyml.util.SwingUtils;
 import space.minecraftstl.xyml.java.JavaRuntime;
+import space.minecraftstl.xyml.setting.SambaException;
+import space.minecraftstl.xyml.setting.SettingsManager;
+import space.minecraftstl.xyml.ui.swing.startup.SwingStartupSafetyDialogs;
 import space.minecraftstl.xyml.util.io.FileUtils;
 import space.minecraftstl.xyml.util.io.JarUtils;
+import space.minecraftstl.xyml.util.platform.CommandBuilder;
 import space.minecraftstl.xyml.util.platform.OperatingSystem;
 import space.minecraftstl.xyml.ui.swing.shell.LauncherIconImages;
 import org.jetbrains.annotations.NotNullByDefault;
@@ -34,7 +38,10 @@ import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import static space.minecraftstl.xyml.util.logging.Logger.LOG;
 import static space.minecraftstl.xyml.util.i18n.I18n.i18n;
@@ -66,6 +73,19 @@ public final class EntryPoint {
         }
 
         enableUnsafeMemoryAccess();
+
+        try {
+            SettingsManager.init();
+        } catch (SambaException e) {
+            showWarning(i18n("fatal.samba"));
+        } catch (IOException e) {
+            LOG.error("Failed to load config", e);
+            checkConfigOwner();
+            SwingUtils.showErrorDialog(i18n(
+                    "fatal.config_loading_failure",
+                    SettingsManager.localConfigDirectory()));
+            exit(1);
+        }
 
         Launcher.main(args);
     }
@@ -203,16 +223,67 @@ public final class EntryPoint {
     /// Warns before running under Wine because native integrations may behave differently.
     private static void checkWine() {
         if (OperatingSystem.isRunningUnderWine()) {
-            SwingUtils.initLookAndFeel();
             LOG.warning("XYML is running under Wine or its distributions!");
-
-            int result = JOptionPane.showOptionDialog(null, i18n("fatal.wine_warning"), i18n("message.warning"), JOptionPane.OK_CANCEL_OPTION,
-                    JOptionPane.WARNING_MESSAGE, null, null, null);
-
-            if (result == JOptionPane.CANCEL_OPTION || result == JOptionPane.CLOSED_OPTION) {
-                exit(1);
-            }
+            showWarning(i18n("fatal.wine_warning"));
         }
+    }
+
+    /// Offers a Unix ownership repair command when the configuration directory is not writable.
+    private static void checkConfigOwner() {
+        if (OperatingSystem.CURRENT_OS == OperatingSystem.WINDOWS)
+            return;
+
+        String userName = Objects.requireNonNullElse(System.getProperty("user.name"), "");
+        Path configDirectory = SettingsManager.localConfigDirectory();
+        if (!Files.exists(configDirectory))
+            return;
+
+        String owner;
+        try {
+            owner = Files.getOwner(configDirectory).getName();
+        } catch (IOException e) {
+            LOG.warning("Failed to get file owner", e);
+            return;
+        }
+
+        if (Files.isWritable(configDirectory) || userName.equals("root") || userName.equals(owner))
+            return;
+
+        ArrayList<String> files = new ArrayList<>();
+        files.add(configDirectory.toString());
+        if (Files.exists(Metadata.XYML_USER_HOME))
+            files.add(Metadata.XYML_USER_HOME.toString());
+
+        Path gameDirectory = Paths.get(".minecraft").toAbsolutePath().normalize();
+        if (Files.exists(gameDirectory))
+            files.add(gameDirectory.toString());
+
+        String command = new CommandBuilder()
+                .addAll("sudo", "chown", "-R", userName)
+                .addAll(files)
+                .toString();
+        SwingStartupSafetyDialogs.offerCopyAndExit(
+                i18n("fatal.config_loading_failure.unix", owner, command),
+                command);
+        exit(1);
+    }
+
+    /// Displays a cancellable warning before the primary Swing runtime is initialized.
+    ///
+    /// @param message localized warning text
+    private static void showWarning(String message) {
+        SwingUtils.initLookAndFeel();
+        int result = JOptionPane.showOptionDialog(
+                null,
+                message,
+                i18n("message.warning"),
+                JOptionPane.OK_CANCEL_OPTION,
+                JOptionPane.WARNING_MESSAGE,
+                null,
+                null,
+                null);
+        if (result == JOptionPane.CANCEL_OPTION || result == JOptionPane.CLOSED_OPTION)
+            exit(1);
     }
 
     /// Enables the JDK compatibility switch required by Java 24 and 25 memory-access warnings.