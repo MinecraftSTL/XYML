HMCL-dev/HMCL

不再用 Version 表示游戏实例 (#6322)

HMCLCore/src/main/java/org/jackhuang/hmcl/download/RemoteVersion.java
@@ -17,7 +17,8 @@
  */
 package org.jackhuang.hmcl.download;
 
-import org.jackhuang.hmcl.game.Version;
+import org.jackhuang.hmcl.game.GameInstanceManifest;
+import org.jackhuang.hmcl.game.GameInstancePatch;
 import org.jackhuang.hmcl.task.Task;
 import org.jackhuang.hmcl.util.ToStringBuilder;
 import org.jackhuang.hmcl.util.versioning.VersionNumber;
@@ -95,7 +96,7 @@ public class RemoteVersion implements Comparable<RemoteVersion> {
         return type;
     }
 
-    public Task<Version> getInstallTask(DefaultDependencyManager dependencyManager, Version baseVersion) {
+    public Task<GameInstancePatch> getInstallTask(DefaultDependencyManager dependencyManager, GameInstanceManifest baseVersion) {
         throw new UnsupportedOperationException(this + " cannot be installed yet");
     }
 

HMCLCore/src/main/java/org/jackhuang/hmcl/download/forge/ForgeInstallProfile.java
@@ -19,7 +19,7 @@ package org.jackhuang.hmcl.download.forge;
 
 import com.google.gson.JsonParseException;
 import com.google.gson.annotations.SerializedName;
-import org.jackhuang.hmcl.game.Version;
+import org.jackhuang.hmcl.game.GameInstanceManifest;
 import org.jackhuang.hmcl.util.Immutable;
 import org.jackhuang.hmcl.util.gson.Validation;
 
@@ -34,9 +34,9 @@ public final class ForgeInstallProfile implements Validation {
     private final ForgeInstall install;
 
     @SerializedName("versionInfo")
-    private final Version versionInfo;
+    private final GameInstanceManifest versionInfo;
 
-    public ForgeInstallProfile(ForgeInstall install, Version versionInfo) {
+    public ForgeInstallProfile(ForgeInstall install, GameInstanceManifest versionInfo) {
         this.install = install;
         this.versionInfo = versionInfo;
     }
@@ -45,7 +45,7 @@ public final class ForgeInstallProfile implements Validation {
         return install;
     }
 
-    public Version getVersionInfo() {
+    public GameInstanceManifest getVersionInfo() {
         return versionInfo;
     }
 

HMCLCore/src/main/java/org/jackhuang/hmcl/launch/Launcher.java
@@ -18,9 +18,9 @@
 package org.jackhuang.hmcl.launch;
 
 import org.jackhuang.hmcl.auth.AuthInfo;
+import org.jackhuang.hmcl.game.GameInstanceManifest;
 import org.jackhuang.hmcl.game.GameRepository;
 import org.jackhuang.hmcl.game.LaunchOptions;
-import org.jackhuang.hmcl.game.Version;
 import org.jackhuang.hmcl.util.platform.ManagedProcess;
 
 import java.io.IOException;
@@ -33,23 +33,23 @@ import java.nio.file.Path;
 public abstract class Launcher {
 
     protected final GameRepository repository;
-    protected final Version version;
+    protected final GameInstanceManifest manifest;
     protected final AuthInfo authInfo;
     protected final LaunchOptions options;
     protected final ProcessListener listener;
     protected final boolean daemon;
 
-    public Launcher(GameRepository repository, Version version, AuthInfo authInfo, LaunchOptions options) {
-        this(repository, version, authInfo, options, null);
+    public Launcher(GameRepository repository, GameInstanceManifest manifest, AuthInfo authInfo, LaunchOptions options) {
+        this(repository, manifest, authInfo, options, null);
     }
 
-    public Launcher(GameRepository repository, Version version, AuthInfo authInfo, LaunchOptions options, ProcessListener listener) {
-        this(repository, version, authInfo, options, listener, true);
+    public Launcher(GameRepository repository, GameInstanceManifest manifest, AuthInfo authInfo, LaunchOptions options, ProcessListener listener) {
+        this(repository, manifest, authInfo, options, listener, true);
     }
 
-    public Launcher(GameRepository repository, Version version, AuthInfo authInfo, LaunchOptions options, ProcessListener listener, boolean daemon) {
+    public Launcher(GameRepository repository, GameInstanceManifest manifest, AuthInfo authInfo, LaunchOptions options, ProcessListener listener, boolean daemon) {
         this.repository = repository;
-        this.version = version;
+        this.manifest = manifest;
         this.authInfo = authInfo;
         this.options = options;
         this.listener = listener;

HMCLCore/src/main/java/org/jackhuang/hmcl/modpack/server/ServerModpackManifest.java
@@ -19,6 +19,7 @@ package org.jackhuang.hmcl.modpack.server;
 
 import com.google.gson.JsonParseException;
 import org.jackhuang.hmcl.download.DefaultDependencyManager;
+import org.jackhuang.hmcl.game.GameInstanceID;
 import org.jackhuang.hmcl.modpack.Modpack;
 import org.jackhuang.hmcl.modpack.ModpackConfiguration;
 import org.jackhuang.hmcl.modpack.ModpackManifest;
@@ -126,8 +127,8 @@ public class ServerModpackManifest implements ModpackManifest, Validation {
                 .orElseThrow(() -> new IOException("Cannot find game version")).getVersion();
         return new Modpack(name, author, version, gameVersion, description, encoding, this) {
             @Override
-            public Task<?> getInstallTask(DefaultDependencyManager dependencyManager, Path zipFile, String name, String iconUrl) {
-                return new ServerModpackLocalInstallTask(dependencyManager, zipFile, this, ServerModpackManifest.this, name);
+            public Task<?> getInstallTask(DefaultDependencyManager dependencyManager, Path zipFile, GameInstanceID instanceId, String iconUrl) {
+                return new ServerModpackLocalInstallTask(dependencyManager, zipFile, this, ServerModpackManifest.this, instanceId);
             }
         };
     }

MinecraftSTL/XYML

XYMLCore/src/main/java/space/minecraftstl/xyml/download/RemoteVersion.java
@@ -17,7 +17,8 @@
  */
 package space.minecraftstl.xyml.download;
 
-import space.minecraftstl.xyml.game.Version;
+import space.minecraftstl.xyml.game.GameInstanceManifest;
+import space.minecraftstl.xyml.game.GameInstancePatch;
 import space.minecraftstl.xyml.task.Task;
 import space.minecraftstl.xyml.util.ToStringBuilder;
 import space.minecraftstl.xyml.util.versioning.VersionNumber;
@@ -95,7 +96,7 @@ public class RemoteVersion implements Comparable<RemoteVersion> {
         return type;
     }
 
-    public Task<Version> getInstallTask(DefaultDependencyManager dependencyManager, Version baseVersion) {
+    public Task<GameInstancePatch> getInstallTask(DefaultDependencyManager dependencyManager, GameInstanceManifest baseVersion) {
         throw new UnsupportedOperationException(this + " cannot be installed yet");
     }
 

XYMLCore/src/main/java/space/minecraftstl/xyml/download/forge/ForgeInstallProfile.java
@@ -19,7 +19,7 @@ package space.minecraftstl.xyml.download.forge;
 
 import com.google.gson.JsonParseException;
 import com.google.gson.annotations.SerializedName;
-import space.minecraftstl.xyml.game.Version;
+import space.minecraftstl.xyml.game.GameInstanceManifest;
 import space.minecraftstl.xyml.util.Immutable;
 import space.minecraftstl.xyml.util.gson.Validation;
 
@@ -34,9 +34,9 @@ public final class ForgeInstallProfile implements Validation {
     private final ForgeInstall install;
 
     @SerializedName("versionInfo")
-    private final Version versionInfo;
+    private final GameInstanceManifest versionInfo;
 
-    public ForgeInstallProfile(ForgeInstall install, Version versionInfo) {
+    public ForgeInstallProfile(ForgeInstall install, GameInstanceManifest versionInfo) {
         this.install = install;
         this.versionInfo = versionInfo;
     }
@@ -45,7 +45,7 @@ public final class ForgeInstallProfile implements Validation {
         return install;
     }
 
-    public Version getVersionInfo() {
+    public GameInstanceManifest getVersionInfo() {
         return versionInfo;
     }
 

XYMLCore/src/main/java/space/minecraftstl/xyml/launch/Launcher.java
@@ -18,9 +18,9 @@
 package space.minecraftstl.xyml.launch;
 
 import space.minecraftstl.xyml.auth.AuthInfo;
+import space.minecraftstl.xyml.game.GameInstanceManifest;
 import space.minecraftstl.xyml.game.GameRepository;
 import space.minecraftstl.xyml.game.LaunchOptions;
-import space.minecraftstl.xyml.game.Version;
 import space.minecraftstl.xyml.util.platform.ManagedProcess;
 
 import java.io.IOException;
@@ -33,23 +33,23 @@ import java.nio.file.Path;
 public abstract class Launcher {
 
     protected final GameRepository repository;
-    protected final Version version;
+    protected final GameInstanceManifest manifest;
     protected final AuthInfo authInfo;
     protected final LaunchOptions options;
     protected final ProcessListener listener;
     protected final boolean daemon;
 
-    public Launcher(GameRepository repository, Version version, AuthInfo authInfo, LaunchOptions options) {
-        this(repository, version, authInfo, options, null);
+    public Launcher(GameRepository repository, GameInstanceManifest manifest, AuthInfo authInfo, LaunchOptions options) {
+        this(repository, manifest, authInfo, options, null);
     }
 
-    public Launcher(GameRepository repository, Version version, AuthInfo authInfo, LaunchOptions options, ProcessListener listener) {
-        this(repository, version, authInfo, options, listener, true);
+    public Launcher(GameRepository repository, GameInstanceManifest manifest, AuthInfo authInfo, LaunchOptions options, ProcessListener listener) {
+        this(repository, manifest, authInfo, options, listener, true);
     }
 
-    public Launcher(GameRepository repository, Version version, AuthInfo authInfo, LaunchOptions options, ProcessListener listener, boolean daemon) {
+    public Launcher(GameRepository repository, GameInstanceManifest manifest, AuthInfo authInfo, LaunchOptions options, ProcessListener listener, boolean daemon) {
         this.repository = repository;
-        this.version = version;
+        this.manifest = manifest;
         this.authInfo = authInfo;
         this.options = options;
         this.listener = listener;

XYMLCore/src/main/java/space/minecraftstl/xyml/modpack/server/ServerModpackManifest.java
@@ -19,6 +19,7 @@ package space.minecraftstl.xyml.modpack.server;
 
 import com.google.gson.JsonParseException;
 import space.minecraftstl.xyml.download.DefaultDependencyManager;
+import space.minecraftstl.xyml.game.GameInstanceID;
 import space.minecraftstl.xyml.modpack.Modpack;
 import space.minecraftstl.xyml.modpack.ModpackConfiguration;
 import space.minecraftstl.xyml.modpack.ModpackManifest;
@@ -126,8 +127,8 @@ public class ServerModpackManifest implements ModpackManifest, Validation {
                 .orElseThrow(() -> new IOException("Cannot find game version")).getVersion();
         return new Modpack(name, author, version, gameVersion, description, encoding, this) {
             @Override
-            public Task<?> getInstallTask(DefaultDependencyManager dependencyManager, Path zipFile, String name, String iconUrl) {
-                return new ServerModpackLocalInstallTask(dependencyManager, zipFile, this, ServerModpackManifest.this, name);
+            public Task<?> getInstallTask(DefaultDependencyManager dependencyManager, Path zipFile, GameInstanceID instanceId, String iconUrl) {
+                return new ServerModpackLocalInstallTask(dependencyManager, zipFile, this, ServerModpackManifest.this, instanceId);
             }
         };
     }